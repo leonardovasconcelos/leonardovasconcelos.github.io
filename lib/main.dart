8cf128462722476fd87c4357a7bc7e23bd889165
Leo <lpiovasconcelos@gmail.com>
2022-08-18 17:39:36 -0300

    Correção para o funcionamento via web

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   final Future<FirebaseApp> _inicialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
